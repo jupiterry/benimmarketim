5bf5b2637e4f62eb872e2a1007cc0c6675ee4018
jupiterry <jupiterryhd@gmail.com>
2025-12-29 05:05:41 +0300

    fix: auth middleware import path

--- backend/routes/weeklyProductRoutes.js
+++ backend/routes/weeklyProductRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { protectRoute, adminRoute } from "../middleware/authMiddleware.js";
+import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 import {
   getWeeklyProducts,
   getAllWeeklyProducts,
